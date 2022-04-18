ae73078e031d5517020d5d2ce60e38d94324d249
joan2g <juanmaortiz3@gmail.com>
2022-04-18 16:19:49 -0300

    Se agrega el proyecto

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require ("express")
+
+const router = require("./routes/productos")
+
+const productRoutes = require("./routes/productos")
+
+const app = express()
+
+app.use(express.static("public"))
+
+app.use(express.json())
+
+app.use(express.urlencoded())
+
+app.use("/api", productRoutes)
+
+app.get("/", (req, res) => {
+    res.sendFile("/public/index.html")
+})
+
+app.listen(8080, () => {
+    console.log("Server is Ok")
+})
+
